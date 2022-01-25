63c7c380d1dd7c356abb9b9edafb52a7aa524104
Fabrice Bascoulergue <lebascou@users.noreply.github.com>
2022-01-25 18:08:29 +0100

    Add new uptime check via crosnest

--- scripts/export_validators.ts
+++ scripts/export_validators.ts
@@ -1,3 +1,4 @@
+import { default as axios } from 'axios';
 import * as csv from 'fast-csv';
 import { LumClient, LumConstants, LumRegistry, LumTypes, LumUtils } from '@lum-network/sdk-javascript';
 import { BondStatus } from '@lum-network/sdk-javascript/build/codec/cosmos/staking/v1beta1/staking';
@@ -58,6 +59,51 @@ const fetchAllDelegationsFrom = async (lumClt: LumClient, addr: string) => {
     return delegations;
 };
 
+const fetchMissedBlocksDailyAvg30Days = async (): Promise<{ [key: string]: number }> => {
+    const res = await axios.post(
+        'https://chain-monitor.cros-nest.com/api/ds/query',
+        JSON.stringify({
+            'queries': [
+                {
+                    'exemplar': true,
+                    'expr': 'chain_operator_misses_new{chain_id="lum-network-1"}',
+                    'interval': '',
+                    'legendFormat': '{{ moniker }}',
+                    'refId': 'A',
+                    'datasource': { 'uid': 'SvF_AYvnz', 'type': 'prometheus' },
+                    'queryType': 'timeSeriesQuery',
+                    'requestId': '2A',
+                    'utcOffsetSec': 0,
+                    'datasourceId': 1,
+                    'intervalMs': 24 * 60 * 60 * 1000,
+                    'maxDataPoints': 100,
+                },
+            ],
+
+            'from': `${new Date().getTime() - 30 * 24 * 60 * 60 * 1000}`,
+            'to': `${new Date().getTime()}`,
+        }),
+        {
+            headers: {
+                'content-type': 'application/json',
+            },
+        },
+    );
+
+    const vals: { [key: string]: number } = {};
+    for (let frame of res.data.results.A.frames) {
+        let missed_blocks = 0;
+        for (let value of frame.data.values[1]) {
+            if (value > 0) {
+                missed_blocks += value;
+            }
+        }
+        vals[frame.schema.fields[1].labels.operator] = missed_blocks / frame.data.values[1].length;
+    }
+
+    return vals;
+};
+
 const main = async () => {
     const lumClt = await LumClient.connect('https://node0.mainnet.lum.network:443/rpc');
 
@@ -65,6 +111,7 @@ const main = async () => {
     const tmValidators = (await lumClt.tmClient.validatorsAll()).validators;
     const stakingValidators = await fetchAllStakingValidators(lumClt);
     const slashingValidators = (await lumClt.queryClient.slashing.signing_infos()).info;
+    const missedBlocksValidators = await fetchMissedBlocksDailyAvg30Days();
 
     const output_validators: Validator[] = [];
 
@@ -73,24 +120,18 @@ const main = async () => {
         const tm_val = tmValidators.filter(
             (v) => LumUtils.Bech32.encode(LumConstants.LumBech32PrefixConsPub, v.pubkey.data) === LumUtils.Bech32.encode(LumConstants.LumBech32PrefixConsPub, cons_pubkey.key),
         )[0];
-        if (!tm_val) {
-            continue;
-        }
-        const slash_val = slashingValidators.filter((v) => v.address === LumUtils.Bech32.encode(LumConstants.LumBech32PrefixConsAddr, tm_val.address))[0];
-        const deleg = delegations.filter((d) => d.delegation.validatorAddress === stake_val.operatorAddress);
 
-        if (deleg.length > 0) {
-            console.log(deleg);
-        }
+        const slash_val = tm_val ? slashingValidators.filter((v) => v.address === LumUtils.Bech32.encode(LumConstants.LumBech32PrefixConsAddr, tm_val.address))[0] : null;
+        const deleg = delegations.filter((d) => d.delegation.validatorAddress === stake_val.operatorAddress);
 
         output_validators.push(
             new Validator(
                 stake_val.description.moniker,
                 stake_val.operatorAddress,
-                slash_val.jailedUntil > new Date(),
+                slash_val ? slash_val.jailedUntil > new Date() : false,
                 stake_val.status === BondStatus.BOND_STATUS_BONDED,
-                slash_val.startHeight.toInt(),
-                slash_val.missedBlocksCounter.toInt(),
+                slash_val ? slash_val.startHeight.toInt() : -1,
+                slash_val ? missedBlocksValidators[slash_val.address] || 0 : 0,
                 parseInt(stake_val.tokens) / 1_000_000,
                 parseFloat(stake_val.commission.commissionRates.rate) / 1_000_000_000_000_000_000,
                 deleg.length === 1 ? parseInt(deleg[0].balance.amount) / 1_000_000 : 0,
@@ -103,9 +144,9 @@ const main = async () => {
 
     const out = csv.format({ delimiter: ',' });
     out.pipe(process.stdout);
-    out.write(['name', 'operator address', 'jailed', 'bonded', 'bonded height', 'missed blocks', 'lum stakes', 'commission', 'lum delegated']);
+    out.write(['name', 'bonded', 'operator address', 'bonded height', 'missed blocks 24h avg', 'lum stakes', 'commission', 'lum delegated']);
     for (let val of output_validators) {
-        out.write([val.name, val.operator_address, val.is_jailed, val.is_bonded, val.bonded_height, val.missed_blocks, val.lum_stakes, val.commission, val.lum_delegation]);
+        out.write([val.name, val.is_bonded, val.operator_address, val.bonded_height, val.missed_blocks, val.lum_stakes, val.commission, val.lum_delegation]);
     }
     out.end();
 };
